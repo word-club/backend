2449129cba0d860482096270b99fd03b582f4490
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-09-30 01:31:58 +0545

    Fixed is owner permission

--- account/permissions.py
+++ account/permissions.py
@@ -3,9 +3,11 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.profile:
+        if obj.profile:  # for profile items
             return obj.profile.user == request.user
-        elif obj.user:
+        elif obj.user:  # for profile
             return obj.user == request.user
+        elif obj.created_by:  # for other models
+            return obj.created_by == request.user
         else:
             return False
