51414f59fda01d96d7beb17d6dcd0b60e3b6c706
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-02 01:29:50 +0545

    Added file size and extension helpers

--- helper.py
+++ helper.py
@@ -2,6 +2,9 @@ import datetime
 import os
 
 from django.utils.timezone import utc
+from rest_framework import serializers
+
+from backend.settings import ALLOWED_IMAGES_EXTENSIONS, MAX_UPLOAD_IMAGE_SIZE
 
 
 def generate_url_for_media_resource(media_url):
@@ -16,3 +19,57 @@ def get_time_diff_in_days(time_posted):
         diff = now - time_posted
         return int(diff.days)
     return 0
+
+
+def check_size(resource, max_size):
+    """
+    Serializer validator
+    Validates file size
+    Raises serializer validation error if requirement does not match
+    """
+    if resource.size / 1000 > max_size:
+        raise serializers.ValidationError(
+            f"Resource exceeds maximum upload size."
+            f" Allowed maximum size: {max_size / 1000} MB"
+        )
+
+
+def check_extension(resource, allowed_extensions_array):
+    """
+    Serializer Validator
+    Validates file extension
+    Raises serializer validation error if requirement does not match
+    """
+    ext = resource.name.split(".")[-1]
+    if ext not in allowed_extensions_array:
+        raise serializers.ValidationError(
+            f"Resource extension '{ext}' is not allowed for upload."
+            f" Allowed extensions are: {', '.join(allowed_extensions_array)}"
+        )
+
+
+def check_images_size_with_ext(images):
+    for image in images:
+        check_extension(image, ALLOWED_IMAGES_EXTENSIONS)
+        check_size(image, MAX_UPLOAD_IMAGE_SIZE)
+
+
+def check_image_size_with_ext(image):
+    check_extension(image, ALLOWED_IMAGES_EXTENSIONS)
+    check_size(image, MAX_UPLOAD_IMAGE_SIZE)
+
+
+# reports: list of reports
+# compares each report's timestamp
+# if a report made withing 15 days is found, returns True
+# else returns False
+def is_recent_report_present(reports):
+    most_recent_report_found = False
+    diff = None
+    for report in reports:
+        diff = get_time_diff_in_days(report.timestamp)
+        if diff < 15:
+            most_recent_report_found = True
+            break
+        diff = None
+    return most_recent_report_found, diff
