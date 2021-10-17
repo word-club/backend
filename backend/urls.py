31affa87e890fd1fa84968ba3af97d738edbdc61
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-10-17 08:55:31 +0545

    Added robots endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.views.static import serve
 
@@ -16,6 +17,11 @@ urlpatterns = [
     path("api/", include("comment.urls")),
     path("api/", include("notification.urls")),
     url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    url(
+        r'^robots.txt',
+        lambda x: HttpResponse("User-Agent: *\nDisallow:", content_type="text/plain"),
+        name="robots_file"
+    ),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
